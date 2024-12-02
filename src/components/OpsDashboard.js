5e253927f4f602dca862f30b5b515ab9a2cbb6a9
akshay124-pixel <akshay124-pixel>
2024-12-02 16:36:45 +0530

    Done

--- src/components/OpsDashboard.js
+++ src/components/OpsDashboard.js
@@ -1265,17 +1265,7 @@ const OpsManagerDashboard = () => {
         {/* Table */}
         <Row>
           {loading ? (
-            <div
-              style={{
-                position: "fixed",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                zIndex: 9999,
-              }}
-            >
-              <Spinner animation="border" variant="primary" />
-            </div>
+            <Spinner animation="border" variant="primary" />
           ) : (
             <Card className="mb-4 shadow-lg">
               <Card.Body>

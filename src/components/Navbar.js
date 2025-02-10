165631ed37c499f30e480952cd5e3b3b3b0dac6b
Akshay Pratap Singh <akshay124-pixel>
2025-02-10 15:00:42 +0530

    Updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -129,35 +129,76 @@ const DashboardNavbar = () => {
   };
 
   return (
-    <Navbar expand="lg" className="flagship-navbar">
+    <Navbar bg="dark" variant="dark" expand="lg" className="shadow-sm">
       <Container>
-        <Navbar.Brand as={Link} to="/" className="navbar-brand">
-          <div className="brand-logo">
-            <i className="fas fa-ticket-alt"></i>
-            <span>TMS</span>
-          </div>
+        <Navbar.Brand className="font-weight-bold">
+          <img
+            src="logo2.png"
+            alt="logo    "
+            width="120"
+            height="42"
+            className="d-inline-block align-top"
+          />
         </Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <i className="fas fa-bars"></i>
-        </Navbar.Toggle>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            {renderNavLinks()}
-            {isAuthenticated && (
+          <Nav className="mx-auto">{renderNavLinks()}</Nav>
+
+          <div className="d-flex align-items-center">
+            {isAuthenticated ? (
               <>
-                <div className="nav-user-info">
-                  <i className="fas fa-user-circle"></i>
-                  <span>{userName}</span>
+                <div className="d-flex align-items-center text-light me-3">
+                  <span>Hello,</span>
+                  <Nav.Link as={Link} to="/profile" className="ms-1 text-light">
+                    {userName}!
+                  </Nav.Link>
                 </div>
-                <button className="logout-button" onClick={handleLogout}>
-                  <i className="fas fa-sign-out-alt"></i>
-                  <span>Logout</span>
+
+                <button class="Btn" onClick={handleLogout}>
+                  <div class="sign">
+                    <svg viewBox="0 0 512 512">
+                      <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
+                    </svg>
+                  </div>
+
+                  <div class="text">Logout</div>
                 </button>
               </>
+            ) : (
+              <>
+                <Button
+                  as={Link}
+                  to="/login"
+                  variant="outline-light"
+                  className="mx-2"
+                  style={{
+                    borderRadius: "20px",
+                    padding: "5px 15px",
+                    fontWeight: "bold",
+                    minWidth: "100px",
+                    height: "38px",
+                  }}
+                >
+                  Login
+                </Button>
+                <Button
+                  as={Link}
+                  to="/signup"
+                  variant="outline-warning"
+                  style={{
+                    borderRadius: "20px",
+                    padding: "5px 15px",
+                    fontWeight: "bold",
+                    minWidth: "100px",
+                    height: "38px",
+                  }}
+                >
+                  Sign Up
+                </Button>
+              </>
             )}
-          </Nav>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
